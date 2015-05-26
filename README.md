7a13d9452ebd904c5d173d9852e49b6b83210b82
crossjs <liwenfu@crossjs.com>
2015-05-26 13:32:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# dong-replace
+
+[![NPM version](https://img.shields.io/npm/v/dong-replace.svg?style=flat-square)](https://npmjs.org/package/dong-replace)
+
+> replace for i18n translation
+
+## Install
+
+```bash
+$ npm install --save dong-replace
+```
+
+## Usage
+
+```js
+vfs.src(view, { dot: false })
+  .pipe(replace(options, dict))
+```
---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var path = require('path')
+
+var gutil = require('gulp-util')
+var through = require('through2')
+
+module.exports = function(options, dict) {
+  options || (options = {})
+
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file)
+      return
+    }
+
+    if (file.isStream()) {
+      cb(new gutil.PluginError('dong-replace', 'Streaming not supported'))
+      return
+    }
+
+    var regexp
+
+    if (dict) {
+      regexp = (function(ext) {
+        switch (ext) {
+          case '.handlebars':
+          case '.html':
+            return /(["'>])([^\n"'><]*[^\x00-\x7f]+?[^\n"'><]*)(["'<])/g
+          case '.js':
+            return /(')([^\n']*[^\x00-\x7f]+?[^\n']*)(\1)/g
+          case '.json':
+            return /(")([^\n"]*[^\x00-\x7f]+?[^\n"]*)(\1)/g
+        }
+      })(path.extname(file.history[0]))
+    }
+
+    var content
+
+    if (regexp) {
+      content = file.contents.toString()
+        .replace(regexp,
+          /*jshint maxparams:4*/
+          function(all, begin, key, end) {
+            if (key in dict) {
+              return begin + dict[key] + end
+            }
+
+            return all;
+          })
+    }
+
+    try {
+      if (content) {
+        file.contents = new Buffer(content)
+      }
+
+      this.push(file)
+    } catch (err) {
+      this.emit('error', new gutil.PluginError('dong-replace', err, {
+        fileName: file.path
+      }))
+    }
+
+    cb()
+  })
+}
